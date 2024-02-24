f322af143bcf0c57a0f5cf12afe4fd762bb0372b
Shubham Kumar <133489048+888Shubham@users.noreply.github.com>
2024-02-24 20:07:42 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async()=>{
     try {
-         await mongoose.connect(process.env.DB_URL);
+         await mongoose.connect("mongodb+srv://shubhamkb888:Shubham%40123@cluster0.a4x67w4.mongodb.net/HospitalAPI");
          console.log("MongoDB connected using mongoose");
     } catch (error) {
         console.log(error);
